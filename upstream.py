b003636a8fbfe0183e82b92d2962c6fab3b3cf3e
四维超立方体 <zhuang-qu@qq.com>
2026-06-12 06:33:56 +0800

    Update upstream.py

--- upstream.py
+++ upstream.py
@@ -11,12 +11,10 @@ LFM4Ads = DCNv2().to(argv[1])
 train(LFM4Ads, "all")
 LFM4Ads.requires_grad_(False)
 
-
 LFM4Ads.CRs = torch.zeros(1000, 6, 360).to(argv[1])
 train_valid_set = pd.concat(Split("all")[:2])
 for _ in infer(LFM4Ads, train_valid_set):
     pass
 LFM4Ads.CRs = torch.nn.functional.layer_norm(LFM4Ads.CRs, [360])
-
 
 torch.save(LFM4Ads, argv[2] + ".pt")
